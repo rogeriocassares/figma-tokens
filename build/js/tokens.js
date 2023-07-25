818feaeca8234cfe06071e32227700b883d78a2c
rogeriocassares <roger10cassares@gmail.com>
2023-07-25 18:39:32 -0300

    add: first figma-token

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 25 Jul 2023 20:56:31 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorPurple500": "#8257e6",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "EffectShadowMd": {"shadowType":"dropShadow","radius":4,"color":"#ffffff1a","offsetX":0,"offsetY":2,"spread":0}
+};
